ccd727f077f72dd73ef51d78a53d88b05128cb45
dev-Lena <keunnalee@gmail.com>
2021-01-26 22:55:39 +0900

    chore: create new SwiftUI project

---
+++ ChartPractice/ChartPractice/ChartPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChartPracticeApp.swift
+//  ChartPractice
+//
+//  Created by Keunna Lee on 2021/01/26.
+//
+
+import SwiftUI
+
+@main
+struct ChartPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

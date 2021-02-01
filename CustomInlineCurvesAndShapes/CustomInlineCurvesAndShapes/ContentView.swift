8604d6e4716c477a1a5949ba0c0dd31b286412e0
dev-Lena <keunnalee@gmail.com>
2021-02-02 00:36:34 +0900

    chore: create project and add assets

---
+++ CustomInlineCurvesAndShapes/CustomInlineCurvesAndShapes/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  CustomInlineCurvesAndShapes
+//
+//  Created by Keunna Lee on 2021/02/02.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
---
+++ CustomInlineCurvesAndShapes/CustomInlineCurvesAndShapes/CustomInlineCurvesAndShapesApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomInlineCurvesAndShapesApp.swift
+//  CustomInlineCurvesAndShapes
+//
+//  Created by Keunna Lee on 2021/02/02.
+//
+
+import SwiftUI
+
+@main
+struct CustomInlineCurvesAndShapesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

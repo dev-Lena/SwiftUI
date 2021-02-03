32b1623b2a846553df72b77a36c39381d9db2db9
dev-Lena <keunnalee@gmail.com>
2021-02-03 15:15:40 +0900

    feat: add utility code
    - SystemImageName, typealias Index

--- CustomInlineCurvesAndShapes/CustomInlineCurvesAndShapes/Utility/SystemImageName.swift
+++ CustomInlineCurvesAndShapes/CustomInlineCurvesAndShapes/Utility/SystemImageName.swift
@@ -9,4 +9,9 @@ import Foundation
 
 enum SystemImageName {
     static let circleGrid = "circle.grid.2x2"
+    static let ellipsis = "ellipsis"
+    static let desktopcomputer = "desktopcomputer"
+    static let quoteBubbleFill = "quote.bubble.fill"
+    static let photoOnRectangle = "photo.on.rectangle"
+    static let scissors =  "scissors"
 }
--- CustomInlineCurvesAndShapes/CustomInlineCurvesAndShapes/Utility/typealias.swift
+++ CustomInlineCurvesAndShapes/CustomInlineCurvesAndShapes/Utility/typealias.swift
@@ -8,3 +8,4 @@
 import Foundation
 
 typealias ImageName = String
+typealias Index = Int
